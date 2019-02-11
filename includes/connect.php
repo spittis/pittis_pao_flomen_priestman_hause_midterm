f7ab33f7879ecd14c0cb85c1ab58417a90bf7622
Soraya Pittis <31996896+spittis@users.noreply.github.com>
2019-02-10 20:37:03 -0500

    folder structure done

---
+++ includes/connect.php
@@ -0,0 +1,13 @@
+<?php
+
+$user = "root";
+$pw = "root";
+
+try {
+    $conn = new PDO('mysql:host=localhost;dbname=INSERTDATABASENAME', $user,$pw);
+    //var_dump($conn); //CHECKING IF CONNECTION WORKS
+} catch(PDOException $exception) {
+    echo 'connect error!' . $exception->getMessage();
+}
+
+?>
