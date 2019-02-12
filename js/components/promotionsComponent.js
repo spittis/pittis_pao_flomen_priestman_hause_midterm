0ec4008d86783a6eebc9f71a066b8066fc23e49a
jaclynflomen <j_floman@fanshaweonline.ca>
2019-02-12 00:05:01 -0500

    text entry

--- js/components/promotionsComponent.js
+++ js/components/promotionsComponent.js
@@ -16,15 +16,15 @@ export default {
     <h2 class="hide" id="whyOntario">Why Choose Ontario</h2>
 
     <ul>
-        <li><a>Explore 1</a>
-            <p>Body TypeLorem ipsum dolor sit amet, <br>consectetuer adipiscing elit, sed diam<br> nonummy nibh euismod tincidunt ut <br>laoreet dolore magna aliquam erat <br>volutpat. Ut wisi enim ad minim <br>veniam, quis nostrud exerci tation.<br><br></p>
-        </li>
-        <li><a>Explore 1</a>
-            <p>Body TypeLorem ipsum dolor sit amet, <br>consectetuer adipiscing elit, sed diam<br> nonummy nibh euismod tincidunt ut <br>laoreet dolore magna aliquam erat <br>volutpat. Ut wisi enim ad minim <br>veniam, quis nostrud exerci tation.<br><br></p>
-        </li>
-        <li><a>Explore 1</a>
-            <p>Body TypeLorem ipsum dolor sit amet, <br>consectetuer adipiscing elit, sed diam<br> nonummy nibh euismod tincidunt ut <br>laoreet dolore magna aliquam erat <br>volutpat. Ut wisi enim ad minim <br>veniam, quis nostrud exerci tation.<br><br></p>
-        </li>
+    <li><a>POSITIVE</a>
+    <p>Ontario Tourism aims to share with the world <br>just how beautiful Ontario is! We are constantly <br>finding new things to love about Ontario.<br> Come see for yourself!<br><br></p>
+</li>
+<li><a>EXCITING</a>
+    <p>There are always new and exciting<br> activities taking place in Ontario that we would <br>love to share! There is no time like the present <br>to come and find excitement!<br><br></p>
+</li>
+<li><a>INVITING</a>
+    <p>Ontario is one of the most inclusive places <br>as everyone is welcome! The door is always <br>open for new and returning residents <br>and visitors. The more the merrier!<br><br></p>
+</li>
     </ul>
 </div>
     
